370a791590075edb30e7fbf65758b498a494d2bb
Aram <boiso.ska8@gmail.com>
2018-11-26 19:44:58 -0600

    modificacion

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
 	public static void main(String args[]){
-	  System.out.println("Mi primer codigoi java subido a github");
+	  System.out.println("Mi primer codigoi java subido a github"); // Modificacion
 	}
 }
